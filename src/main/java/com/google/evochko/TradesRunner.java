83e55dd1af1bf4c3377f5f12484be79593f27be4
syevochko <evochko@ukr.net>
2019-12-19 00:17:15 +0200

    first commit

---
+++ src/main/java/com/google/evochko/TradesRunner.java
@@ -0,0 +1,10 @@
+package com.google.evochko;
+
+/**
+ * Hello world!
+ */
+public class TradesRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
